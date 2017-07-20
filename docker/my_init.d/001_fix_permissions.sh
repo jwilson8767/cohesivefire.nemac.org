0840c6513a1122496686987815fc1eeb08b54965
Joshua Wilson <jwilson@waystone.systems>
2017-07-20 12:46:16 -0400

    Permission fixes

--- docker/my_init.d/001_fix_permissions.sh
+++ docker/my_init.d/001_fix_permissions.sh
@@ -7,7 +7,7 @@ set -x
 # Fixes most shared folder permission issues.
 if [[ `stat -c "%u" /app` != 0 ]]; then
   # By changing apache's uid we avoid having to chown all files /app
-  usermod --non-unique --uid `stat -c "%u" /app` apache
+  usermod --non-unique --uid `stat -c "%u" /app` -gid `stat -c "%g" /app`  apache
 else
   # Root owns the folder, best to just chown.
   chown apache:apache /app -R
--- docker/my_init.d/004_composer_install.sh
+++ docker/my_init.d/004_composer_install.sh
@@ -2,13 +2,14 @@
 #
 # This script is run when the container is first started.
 #
-
+export COMPOSER_ALLOW_SUPERUSER=1
 composer global require hirak/prestissimo
 
-chown apache:apache /.composer
+chown -R apache:apache /opt/.composer
 
 #run composer install
 su -s "/bin/bash" -c "cd /app/ && composer install" apache
 
+mkdir -p /app/web/sites/default
 chmod 660 /app/web/sites/default/settings.php 2> /dev/null
 echo "<?php require(dirname(\$app_root) . '/settings.php');" > /app/web/sites/default/settings.php
